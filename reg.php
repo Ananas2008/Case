a973c7c30be38873b5cf7e1cc8aed60b2db1dd65
Ananas2008 <99872221+Ananas2008@users.noreply.github.com>
2022-02-17 12:42:49 +0300

    First commit

---
+++ reg.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./css/reg.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student</title>
+</head>
+<body>
+
+     <form action="">
+        <input type="name" required="" placeholder="Имя">
+         <input type="login" required="" placeholder="Логин">
+         <input type="password" required="" placeholder="Пароль">
+         <input type="email" required="" placeholder="Почта">
+         <input type="checkbox" required="" value="1680" > Согласие на обработку персональный данных
+         <button>Войти</button>
+       </form>
+       
+
+    </div>
+  </div>
+  
+</body>
+</html>
